23cf78366686b6d66785086a81b09abaff81adfb
Naoyuki Sogo <sogo.naoyuki@foskia-image.co.jp>
2026-06-16 12:51:43 +0900

    データ更新: 2026-06-16 - 9件追加

--- data.js
+++ data.js
@@ -3,9 +3,162 @@
 // フォーマット: window.presentsData = [ ...entries ]
 
 window.presentsData = [
+  {
+    id: 'tokyofm-madocafe-20260630',
+    stationId: 'tokyofm',
+    station: 'TOKYO FM',
+    program: '皆藤愛子の窓café ～窓辺でcafé time～',
+    programUrl: 'https://www.tfm.co.jp/madocafe/',
+    prize: 'THERMOS COOL遮熱 日傘 軽量タイプ シルバー（毎週1名様）',
+    deadline: '2026-06-30',
+    applyUrl: 'https://www.tfm.co.jp/madocafe/index.php?catid=3429',
+    applyMethod: '公式サイトから応募',
+    sns: {
+      twitter: null,
+      instagram: null,
+      web: 'https://www.tfm.co.jp/madocafe/'
+    },
+    addedDate: '2026-06-16'
+  },
+  {
+    id: 'tokyofm-ise2026-20260716',
+    stationId: 'tokyofm',
+    station: 'TOKYO FM',
+    program: 'TOKYO FM 伊勢特集',
+    programUrl: null,
+    prize: '「伊勢おたのしみセット」と「お伊勢るるるん」詰め合わせ（10名様）',
+    deadline: '2026-07-16',
+    applyUrl: 'https://www.tfm.co.jp/bo/ise2026/',
+    applyMethod: '公式サイトから応募',
+    sns: {
+      twitter: null,
+      instagram: null,
+      web: 'https://www.tfm.co.jp/bo/ise2026/'
+    },
+    addedDate: '2026-06-16'
+  },
+  {
+    id: 'tokyofm-agripower-20260630',
+    stationId: 'tokyofm',
+    station: 'TOKYO FM',
+    program: 'あぐりずむ 未来へつなぐあぐりパワー',
+    programUrl: 'https://www.tfm.co.jp/agripower/',
+    prize: 'JAタウンギフトカード5,000円分（毎月5名様）',
+    deadline: '2026-06-30',
+    applyUrl: 'https://www.tfm.co.jp/f/agripower/form',
+    applyMethod: '公式サイトから応募',
+    sns: {
+      twitter: null,
+      instagram: null,
+      web: 'https://www.tfm.co.jp/agripower/'
+    },
+    addedDate: '2026-06-16'
+  },
+  {
+    id: 'tokyofm-sol-20260716',
+    stationId: 'tokyofm',
+    station: 'TOKYO FM',
+    program: 'SCHOOL OF LOCK! 放送部 君だけの歌 supported by JASRAC',
+    programUrl: 'https://www.tfm.co.jp/lock/',
+    prize: 'オリジナルQUOカードPay 3,000円分',
+    deadline: '2026-07-16',
+    applyUrl: 'https://www.tfm.co.jp/lock/housou/',
+    applyMethod: '公式サイトから応募',
+    sns: {
+      twitter: null,
+      instagram: null,
+      web: 'https://www.tfm.co.jp/lock/'
+    },
+    addedDate: '2026-06-16'
+  },
+  {
+    id: 'tokyofm-shiningstar-20260716',
+    stationId: 'tokyofm',
+    station: 'TOKYO FM',
+    program: 'SHINING STAR',
+    programUrl: null,
+    prize: 'ブルボン「プチ24本詰合せBOX」（10名様）',
+    deadline: '2026-07-16',
+    applyUrl: 'https://form.audee.jp/shiningstar/message',
+    applyMethod: '公式サイトから応募',
+    sns: {
+      twitter: null,
+      instagram: null,
+      web: null
+    },
+    addedDate: '2026-06-16'
+  },
+  {
+    id: 'jwave-blueingreen-20260716',
+    stationId: 'jwave',
+    station: 'J-WAVE',
+    program: 'BLUE IN GREEN',
+    programUrl: 'https://www.j-wave.co.jp/original/blueingreen/',
+    prize: 'モンカフェ合計42杯分スペシャルアソートセット＋番組オリジナルグッズ（抽選15名様）',
+    deadline: '2026-07-16',
+    applyUrl: 'https://www.j-wave.co.jp/original/blueingreen/present.html',
+    applyMethod: '公式サイトから応募',
+    sns: {
+      twitter: null,
+      instagram: null,
+      web: 'https://www.j-wave.co.jp/original/blueingreen/'
+    },
+    addedDate: '2026-06-16'
+  },
+  {
+    id: 'interfm-radioradio-20260716',
+    stationId: 'interfm',
+    station: 'InterFM897',
+    program: 'RADIO. RADIO. With George Williams',
+    programUrl: null,
+    prize: 'Amazonギフトカード10,000円分（2名様）',
+    deadline: '2026-07-16',
+    applyUrl: null,
+    applyMethod: 'スーパーリアルタイムリクエスト採用時',
+    sns: {
+      twitter: null,
+      instagram: null,
+      web: 'https://www.interfm.co.jp/'
+    },
+    addedDate: '2026-06-16'
+  },
+  {
+    id: 'interfm-lazysunday-20260716',
+    stationId: 'interfm',
+    station: 'InterFM897',
+    program: 'Lazy Sunday',
+    programUrl: null,
+    prize: '「WINES N\'S ROSES」ワイン（5名様）',
+    deadline: '2026-07-16',
+    applyUrl: null,
+    applyMethod: '恒例のクイズによる応募',
+    sns: {
+      twitter: null,
+      instagram: null,
+      web: 'https://www.interfm.co.jp/'
+    },
+    addedDate: '2026-06-16'
+  },
+  {
+    id: 'interfm-barakanbeat-20260716',
+    stationId: 'interfm',
+    station: 'InterFM897',
+    program: 'Barakan Beat',
+    programUrl: null,
+    prize: '南会津マウンテンブルーイングクラフトビール5本セット（3名様）',
+    deadline: '2026-07-16',
+    applyUrl: null,
+    applyMethod: '番組への応募',
+    sns: {
+      twitter: null,
+      instagram: null,
+      web: 'https://www.interfm.co.jp/'
+    },
+    addedDate: '2026-06-16'
+  },
 ];
 
 window.siteConfig = {
   lastUpdated: '2026-06-16',
-  totalCount: 0
+  totalCount: 9
 };
